4535cfe1b15212aa24490bf7e5af114c319d7a70
murali <murali@banyandata.com>
2022-09-24 09:25:30 +0530

    Update runapp.sh

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Starting petclinic app
 cd /tmp
-sudo nohup java -jar spring-petclinic-2.2.0.BUILD-SNAPSHOT.jar > /home/ec2-user/petclinic.log 2>&1 &
+sudo nohup java -jar spring-petclinic-*.jar > /home/ec2-user/petclinic.log 2>&1 &
